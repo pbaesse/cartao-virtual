7a30b02f5b4b211eb526660ed8df75d2a63bf44b
ytalojhonata <you@example.com>
2019-09-22 17:49:15 -0300

    adição de aba para organização do template utilizando auxilio do bootstrap

---
+++ resources/views/template/barranav.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="{{ url('/modeloteste') }}">cartão virtual</a>
+    </div>
+    <ul class="nav navbar-nav">
+      
+     
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="{{ url('/home') }}"><span class="glyphicon glyphicon-cog"></span> Configurar site</a></li>
+      
+    </ul>
+  </div>
+</nav>
---
+++ resources/views/template/rodape.blade.php
@@ -0,0 +1,3 @@
+<footer class="container-fluid bg-4 text-center">
+  <p>Cartão virtual</p> 
+</footer>
