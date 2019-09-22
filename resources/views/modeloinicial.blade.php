7813160347165be47ab590f081c68ffb6045ee48
ytalojhonata <you@example.com>
2019-09-22 17:47:43 -0300

    nova view para modelo inicial da página.

---
+++ resources/views/modeloinicial.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+@extends('template.head')
+<style type="text/css">
+	body {
+    font: 20px Montserrat, sans-serif;
+    line-height: 1.8;
+    color: #f5f6f7;
+  }
+    p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+    background-color: #1abc9c; 
+    color: #333;
+  }
+  .bg-2 { 
+    background-color: #474e5d; 
+    color: #ffffff;
+  }
+  .bg-3 { 
+    background-color: #ffffff; 
+    color: #555555;
+  }
+  .bg-4 { 
+    background-color: #2f2f2f; 
+    color: #fff;
+  }
+  .navbar.navbar-inverse {
+  	margin-bottom: 0px;
+  }
+  .img.responsive.margin{
+    width: 550px;
+    height: 200px;
+  }
+  
+</style>
+<body>
+@extends('template.rodape')
+@extends('template.container3')
+@extends('template.container2')
+@extends('template.container1')
+@extends('template.barranav')
+</body>
+</html>
