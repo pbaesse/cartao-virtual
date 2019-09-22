23d6c4d070910a358a66085bc6e5986646b86bf1
ytalojhonata <you@example.com>
2019-09-22 17:44:43 -0300

    novas rotas adicionadas a página

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/',('PagController@inicio'));
+Route::get('/modelos',('PagController@escolha'));
+Route::get('/modeloteste',('PagController@modeloteste'));
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
