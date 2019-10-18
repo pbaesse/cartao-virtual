1023eea1d10610e86b30ca672c9b5a7b9f5dee0c
Rodrigo-18 <rss.174@hotmail.com>
2019-10-17 21:28:52 -0300

    alterando os background's das telas de login e de registro

--- resources/views/modelo2v.blade.php
+++ resources/views/modelo2v.blade.php
@@ -93,7 +93,7 @@
 
     <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
       <div class="w-100">
-        <h2 class="mb-5">Experiencias</h2>
+        <h2 class="mb-5">{{$dat}}</h2>
 
         <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="resume-content">
