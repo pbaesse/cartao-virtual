db99211b30a9bb832e34d127782a8ea5071efc2b
ytalojhonata <you@example.com>
2019-10-20 17:37:22 -0300

    Mudança para ser obrigatorio adicionar os dados

--- resources/views/dados_users.blade.php
+++ resources/views/dados_users.blade.php
@@ -15,52 +15,88 @@
   <!-- Libraries CSS Files -->
   
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="/lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
 
 
   <!-- Main Stylesheet File -->
-  <link href="/css/style.css" rel="stylesheet">
-  <link href="/css/dados.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/dados.css" rel="stylesheet">
 
 </head>
 <body>
  
 	<div class="dados" >
-	<form method="POST" action="{{ route('form.store') }}">
+	<form method="POST" action="{{ route('form.store') }}" class="needs-validation" novalidate>
     {!! csrf_field() !!}
     <h1></h1>
     
     <input type="text" hidden name="user_id" value="{{$id_user}}">
 
   <div class="form-group">
-      <label >Nome completo</label>
-      <input type="text" class="form-control" name="nome" id="validationDefault01" placeholder="Nome" >
+  
+      <label for="validationCustom03">Nome Completo</label>
+      <input type="text" class="form-control" name="nome" id="validationCustom03" placeholder="Nome" required>
+      <div class="invalid-feedback">
+        Campo Obrigatorio!
+      </div>
     </div>
  <div class="form-group">
   <div class="form-row">
     <div class="col-md-6 mb-3">
-      <label for="validationDefault03">Cidade</label>
-      <input type="text" class="form-control" name="cidade" id="cidade" placeholder="Cidade" >
+      <label for="validationCustom03">Cidade</label>
+      <input type="text" class="form-control" name="cidade" id="validationCustom03" placeholder="Cidade" required>
+      <div class="invalid-feedback">
+        Campo Obrigatorio!
+      </div>
     </div>
     <div class="col-md-6 mb-3">
-      <label for="validationDefault03">Data de Nacimento</label>
-      <input type="date" class="form-control" name="data_nasc" id="date" placeholder="" >
+      <label for="validationCustom04">Data de nascimento</label>
+     <input type="date" class="form-control" name="data_nasc" id="validationCustom04" id="date" placeholder="" required>
+      <div class="invalid-feedback">
+        Campo Obrigatorio!
+      </div>
     </div>
     
     
   </div>
   <div class="form-group">
-    <label for="testeAreaDescr">Hobbies</label>
-    <textarea class="form-control" id="description" name="hobbies" rows="3" placeholder="Informe seus hobbies aqui"></textarea>
+    <label for="validationCustom05">Hoobies</label>
+    <textarea class="form-control" id="description" id="validationCustom05" name="hobbies" rows="3" placeholder="Informe seus hobbies aqui" required ></textarea>
+     <div class="invalid-feedback">
+        Campo Obrigatorio!
+      </div>
   </div>
   <div class="form-group">
     <label for="testeAreaDescr">Descrição</label>
-    <textarea class="form-control" id="description" name="descricao" rows="3" placeholder="Faça uma descrição sobre você"></textarea>
+    <textarea class="form-control" id="description" name="descricao" rows="3" placeholder="Faça uma descrição sobre você" required></textarea>
+    <div class="invalid-feedback">
+        Campo Obrigatorio!
+      </div>
   </div>
   <button class="btn btn-primary" type="submit">Concluir</button>
 </div>
 </form>
 	</div>
+  <script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
 
 
 
